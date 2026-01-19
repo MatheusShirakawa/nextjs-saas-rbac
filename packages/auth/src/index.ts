4a54ba8ac03d609a4e980be9d8cdf8f091e048a8
MatheusShirakawa <matheus_shirakawa@outlook.com>
2026-01-19 18:29:09 -0300

    raw: :bricks: adding all the schemas with zod and casl to deal with permissions

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,46 @@
+import {
+    AbilityBuilder,
+    createMongoAbility,
+  } from '@casl/ability';
+import { z } from 'zod';
+import type { CreateAbility, MongoAbility } from '@casl/ability';
+import type { User } from './models/user';
+import { permissions } from './permissions';
+import { userSubject } from './subjects/user';
+import { projectSubject  } from './subjects/project';
+import { organizationSubject } from './subjects/organization';
+import { billingSubject } from './subjects/billing';
+import { inviteSubject } from './subjects/invite';
+
+// Define the allowed actions and subjects as a tuple type
+const appAbilitiesSchema = z.union([
+  projectSubject,
+  userSubject,
+  organizationSubject,
+  billingSubject,
+  inviteSubject,
+  z.tuple([z.literal('manage'), z.literal('all')])
+])
+
+type AppAbilities = z.infer<typeof appAbilitiesSchema>;
+
+export type AppAbility = MongoAbility<AppAbilities>;
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>;
+
+export function defineAbilityFor(user: User){
+  const builder = new AbilityBuilder(createAppAbility)
+
+  if (typeof permissions[user.role] !== 'function'){
+    throw new Error(`No permissions defined for role "${user.role}"`)
+  }
+
+  permissions[user.role](user, builder)
+
+  const ability = builder.build({
+    detectSubjectType(subject){
+      return subject.__typename
+    }
+  })
+
+  return ability
+}
